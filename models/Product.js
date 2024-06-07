0e7a3f691c00e70985436b64cfee91a5da8eb931
manna-avijit <a.manna1202@gmail.com>
2024-06-07 22:19:11 +0530

    Update product model to include additional attributes
    Add supplier, sales, price, and quantity attributes to product model

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    }
+  name: { type: String, required: true },
+  description: String,
+  supplier: String,
+  sales: Number,
+  price: Number,
+  quantity: Number
 });
 
-module.exports = mongoose.model('Product', productSchema);
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
